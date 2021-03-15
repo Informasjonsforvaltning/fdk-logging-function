dae7cb76d42a9ebe9542b45b6b07c3ecf56bdfe7
matsjs <matsjorgen@hotmail.com>
2021-03-15 10:38:48 +0100

    feat: adds cors

--- fdk_logging_function.go
+++ fdk_logging_function.go
@@ -30,6 +30,13 @@ func Logging(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
 		logger(w, r)
+	case http.MethodOptions:
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
 	default:
 		http.Error(w, "405 - Method Not Allowed", http.StatusMethodNotAllowed)
 	}
